ab1c1e7fb56b46c6f8f0b173acc1a0077e29bcd6
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2025-05-22 22:45:29 +0800

    feat: number key shortcuts

--- src/routes/test/[test]/+page.svelte
+++ src/routes/test/[test]/+page.svelte
@@ -2,6 +2,8 @@
 	import { goto } from '$app/navigation';
 	import type { PageProps } from './$types';
 
+	import Choice from './Choice.svelte';
+
 	import { encode } from '$lib/codec';
 	import { shuffle } from '$lib/utils';
 	import type { Marker, Factor } from '$lib/data';
@@ -22,6 +24,32 @@
 		shuffledMarkers = shuffle(data.markers);
 	});
 
+	let forceActive = $state(Array.from<boolean>({ length: 5 }).fill(false));
+
+	const keydownListener = (ev: KeyboardEvent) => {
+		if (ev.isComposing) return;
+
+		// eslint-disable-next-line unicorn/prefer-switch
+		if (ev.key === '1') forceActive[0] = true;
+		else if (ev.key === '2') forceActive[1] = true;
+		else if (ev.key === '3') forceActive[2] = true;
+		else if (ev.key === '4') forceActive[3] = true;
+		else if (ev.key === '5') forceActive[4] = true;
+	};
+
+	const keyupListener = (ev: KeyboardEvent) => {
+		if (ev.isComposing) return;
+
+		// eslint-disable-next-line unicorn/prefer-switch
+		if (ev.key === '1') respond(1);
+		else if (ev.key === '2') respond(2);
+		else if (ev.key === '3') respond(3);
+		else if (ev.key === '4') respond(4);
+		else if (ev.key === '5') respond(5);
+
+		forceActive.fill(false);
+	};
+
 	const respond = (r: 1 | 2 | 3 | 4 | 5) => {
 		const currentMarker = shuffledMarkers?.at(-1);
 		if (shuffledMarkers === undefined || currentMarker === undefined) return;
@@ -41,6 +69,8 @@
 	<title>Test / Big-Five Factor Markers</title>
 </svelte:head>
 
+<svelte:window onkeydown={keydownListener} onkeyup={keyupListener} />
+
 {#if shuffledMarkers !== undefined && shuffledMarkers.at(-1) !== undefined}
 	<p>
 		{data.markers.length - shuffledMarkers.length + 1}<span class="text-foreground-3"
@@ -50,25 +80,20 @@
 	<p class="text-pretty">{shuffledMarkers.at(-1)!.text}</p>
 
 	<div class="mt-8 flex w-full shrink-0 flex-col gap-2 lg:flex-row">
-		<button
-			class="bg-background-2 hover:bg-primary hover:text-background active:text-background active:bg-primary-2 w-full px-8 py-4 text-sm hover:cursor-pointer"
-			onclick={() => respond(5)}>Strongly agree</button
-		>
-		<button
-			class="bg-background-2 hover:bg-primary hover:text-background active:text-background active:bg-primary-2 w-full px-8 py-4 text-sm hover:cursor-pointer"
-			onclick={() => respond(4)}>Agree</button
-		>
-		<button
-			class="bg-background-2 hover:bg-primary hover:text-background active:text-background active:bg-primary-2 w-full px-8 py-4 text-sm hover:cursor-pointer"
-			onclick={() => respond(3)}>Neutral</button
-		>
-		<button
-			class="bg-background-2 hover:bg-primary hover:text-background w-full px-8 py-4 text-sm hover:cursor-pointer"
-			onclick={() => respond(2)}>Disagree</button
-		>
-		<button
-			class="bg-background-2 hover:bg-primary hover:text-background w-full px-8 py-4 text-sm hover:cursor-pointer"
-			onclick={() => respond(1)}>Strongly disagree</button
-		>
+		<Choice
+			label="Strongly disagree"
+			shortcut="1"
+			forceActive={forceActive[0]}
+			onclick={() => respond(1)}
+		/>
+		<Choice label="Disagree" shortcut="2" forceActive={forceActive[1]} onclick={() => respond(2)} />
+		<Choice label="Neutral" shortcut="3" forceActive={forceActive[2]} onclick={() => respond(3)} />
+		<Choice label="Agree" shortcut="4" forceActive={forceActive[3]} onclick={() => respond(4)} />
+		<Choice
+			label="Strongly agree"
+			shortcut="5"
+			forceActive={forceActive[4]}
+			onclick={() => respond(5)}
+		/>
 	</div>
 {/if}
---
+++ src/routes/test/[test]/Choice.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	const {
+		label,
+		shortcut,
+		forceActive = false,
+		...restProps
+	}: {
+		label: string;
+		shortcut: string;
+		forceActive?: boolean;
+	} & HTMLButtonAttributes = $props();
+</script>
+
+<button
+	class={[
+		'group bg-background-2 hover:bg-primary hover:text-background active:text-background active:bg-primary-2 relative w-full px-8 py-4 text-sm hover:cursor-pointer',
+		forceActive ? 'text-background bg-primary' : null,
+	]}
+	{...restProps}
+>
+	<span>{label}</span>
+	<span
+		class={[
+			'text-foreground-3 group-hover:text-background group-active:text-background absolute top-0 left-0 m-1.5 text-[10px]',
+			forceActive ? '!text-background' : null,
+		]}>{shortcut}</span
+	>
+</button>
